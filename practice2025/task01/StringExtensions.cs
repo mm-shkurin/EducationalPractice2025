e7a1454752b14115ec1566ca90abea12557bace6
mm-shkurin <trape3977@gmail.com>
2025-07-16 20:54:25 +0600

    feat:add an extension function

---
+++ practice2025/task01/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace task01;
+public static class StringExtensions{
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            var filtered = new System.Text.StringBuilder();
+            foreach (char c in input)
+            {
+                if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                {
+                    filtered.Append(char.ToLower(c));
+                }
+            }
+            
+            if (filtered.Length == 0)
+                return false;
+
+            int left = 0;
+            int right = filtered.Length - 1;
+            
+            while (left < right)
+            {
+                if (filtered[left] != filtered[right])
+                    return false;
+                
+                left++;
+                right--;
+            }
+            
+            return true;
+        }
+}
