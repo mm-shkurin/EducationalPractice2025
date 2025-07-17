ff76a5a2d3ffaef8ede985e268c783aedee3c3c3
mm-shkurin <trape3977@gmail.com>
2025-07-17 11:25:41 +0600

    feat:add Class Attributes and Reflection in C#

---
+++ practice2025/task07/DisplayNameAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
+public class DisplayNameAttribute : Attribute
+{
+    public string DisplayName { get; }
+    public DisplayNameAttribute(string displayName)
+    {
+        DisplayName = displayName;
+    }
+}
