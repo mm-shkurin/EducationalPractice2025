3da60d9d234778270785879da303eec41fa50eae
mm-shkurin <trape3977@gmail.com>
2025-07-17 10:51:50 +0600

    refactor:based on the assignment

--- practice2025/task03/CustomCollection.cs
+++ practice2025/task03/CustomCollection.cs
@@ -6,7 +6,6 @@ using System.Linq;
 public class CustomCollection<T> : IEnumerable<T>
 {
     private readonly List<T> _items = new();
-
     public void Add(T item) => _items.Add(item);
     public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
